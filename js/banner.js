d94ac85518ca6055e2d5165eb7931149146e6ad7
zzeondev <zzeon.dev@gmail.com>
2025-07-07 18:13:18 +0900

    [docs] 배너진행중

--- js/banner.js
+++ js/banner.js
@@ -1,10 +1,22 @@
 window.addEventListener("DOMContentLoaded", function () {
   var swiper = new Swiper(".sw_banner", {
-    slidesPerView: "auto",
+    slidesPerView: 1,
     spaceBetween: 80,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: ".banner_slide_next",
+      prevEl: ".banner_slide_prev",
+    },
     pagination: {
-      el: ".swiper-pagination",
+      el: ".banner_slide_pg",
       clickable: true,
+      bulletClass: "swiper-pagination-bullet",
+      bulletActiveClass: "swiper-pagination-bullet-active",
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
     },
   });
 });
